e9257d4e87b96a27d49aaa82d6db12d761adfde0
Sharmina Akhtar <sakht318@gmail.com>
2018-04-16 22:44:14 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,9 +28,8 @@ class PostsController < ApplicationController
 	end
 
 	private
- 
-def post_params
-  params.require(:post).permit(:title, :description)
-end
-end
 
+		def post_params
+		  params.require(:post).permit(:title, :description)
+		end
+end
